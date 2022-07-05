c7cc1f3e21212d9c63c476f66f6ae6fe3fd35a0e
Cesar Mendoza <ingcesarmendoza@gmail.com>
2022-07-05 12:09:38 -0500

    Version 1.0

---
+++ RobocopyST.ps1
@@ -0,0 +1,30 @@
+# **************************************************************************************************
+#  Robocopy Scripting Tool
+# **************************************************************************************************
+# AUTOR: Cesar Mendoza
+# **************************************************************************************************
+# Herramienta sencilla para crear comandos de robocopy por cada subcarpeta de una carpeta raíz. 
+# **************************************************************************************************
+$rootFolder = "C:\Projects"; 
+$scriptsFolder = "C:\Projects\Scripts"; 
+$scriptsExtension = ".txt"; 
+$logsFolder = "C:\Projects\Logs"; 
+$logsExtension = ".txt"; 
+
+$sourceFolder = "\\machine1\folder1"; 
+$destFolder = "\\machine2\folder1"; 
+
+Write-Information "Inicia el script "; 
+
+$logsExtension = "-" + (Get-Date).Year + "-" + (Get-Date).Month + "-" + (Get-Date).Day + "-" + (Get-Date).Hour + "-" + (Get-Date).Minute + "-LOG" + $logsExtension; 
+
+$subFolders = Get-ChildItem -Path $rootFolder -Directory
+
+foreach ($folder in $subFolders) {
+    $logPath = $logsFolder + "\" + $folder + $logsExtension; 
+    $command = 'Robocopy "' + $sourceFolder + '\' + $folder + '" "' + $destFolder + '\' + $folder + '" /TEE /S /E /COPYALL /PURGE /ZB /NP /R:2 /W:1 /LOG:"' + $logPath + '"'; 
+    $scriptPath = $scriptsFolder + "\" + $folder + $scriptsExtension;     
+    $command | Out-File -FilePath $scriptPath -Encoding unicode
+}
+
+Write-Information "Script finalizado."; 
