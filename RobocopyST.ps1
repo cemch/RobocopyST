d56168617d263ccca6519fe699c5036488a3ab82
Cesar Mendoza <ingcesarmendoza@hotmail.com>
2022-07-30 16:55:27 -0500

    Se mejoran los parametros de copia

--- RobocopyST.ps1
+++ RobocopyST.ps1
@@ -5,24 +5,26 @@
 # **************************************************************************************************
 # Herramienta sencilla para crear comandos de robocopy por cada subcarpeta de una carpeta raíz. 
 # **************************************************************************************************
-$rootFolder = "C:\Projects"; 
-$scriptsFolder = "C:\Projects\Scripts"; 
+# PARAMETROS: 
+$sourceFolder = "\\localhost\source"; # Nunca finalice con \
+$targetFolder = "\\localhost\target"; # Nunca finalice con \
+$params = '/TEE /S /E /COPYALL /PURGE /ZB /NP /R:2 /W:1 /LOG:"' # /LOG:Siempre al final. 
+
+$scriptsFolder = "C:\Robocopy\Scripts"; # Nunca finalice con \
 $scriptsExtension = ".txt"; 
-$logsFolder = "C:\Projects\Logs"; 
+$logsFolder = "C:\Robocopy\Logs"; # Nunca finalice con \
 $logsExtension = ".txt"; 
-
-$sourceFolder = "\\machine1\folder1"; 
-$destFolder = "\\machine2\folder1"; 
-
+# **************************************************************************************************
+# INICIA EL SCRIPT
 Write-Information -MessageData "Inicia el script " -InformationAction Continue; 
 
 $logsExtension = "-" + (Get-Date).Year + "-" + (Get-Date).Month + "-" + (Get-Date).Day + "-" + (Get-Date).Hour + "-" + (Get-Date).Minute + "-LOG" + $logsExtension; 
 
-$subFolders = Get-ChildItem -Path $rootFolder -Directory
+$subFolders = Get-ChildItem -Path $sourceFolder -Directory
 
 foreach ($folder in $subFolders) {
     $logPath = $logsFolder + "\" + $folder + $logsExtension; 
-    $command = 'Robocopy "' + $sourceFolder + '\' + $folder + '" "' + $destFolder + '\' + $folder + '" /TEE /S /E /COPYALL /PURGE /ZB /NP /R:2 /W:1 /LOG:"' + $logPath + '"'; 
+    $command = 'Robocopy "' + $sourceFolder + '\' + $folder + '" "' + $targetFolder + '\' + $folder + '" ' + $params + $logPath + '"'; 
     $scriptPath = $scriptsFolder + "\" + $folder + $scriptsExtension;     
     $command | Out-File -FilePath $scriptPath -Encoding unicode
     Write-Information -MessageData "Creando robocopy para la carpeta $folder" -InformationAction Continue; 
